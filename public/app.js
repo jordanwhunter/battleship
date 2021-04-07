8a5dd03213ed578e0b050b27e2e591f34297bfd2
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-04-07 18:02:55 -0400

    Created shipsArray

--- public/app.js
+++ public/app.js
@@ -17,18 +17,69 @@ document.addEventListener("DOMContentLoaded", () => {
   const infoDisplay = document.querySelector("#info");
 
   const userSquares = [];
+  const computerSquares = [];
 
   const width = 10;
 
   // Create board
   const createBoard = (grid, squares, width) => {
-    for (let i = 0; i < width*width; i++ ) {
+    for (let i = 0; i < width * width; i++ ) {
       const square = document.createElement("div");
       square.dataset.id = i;
       grid.appendChild(square);
-      squares.push(square)
+      squares.push(square);
     };
   };
 
-  createBoard(userGrid, userSquares, width);
+  createBoard(userGrid, userSquares);
+  createBoard(computerGrid, computerSquares);
+
+  // Array of ships for random assortment
+  const shipsArray = [
+    {
+      name: "destroyer",
+      directions: [
+        // horizontal
+        [0, 1],
+        // vertical
+        [0, width]
+      ]
+    },
+    {
+      name: "submarine",
+      directions: [
+        // horizontal
+        [0, 1, 2],
+        // vertical
+        [0, width, width * 2]
+      ]
+    },
+    {
+      name: "cruiser",
+      directions: [
+        // horizontal
+        [0, 1, 2],
+        // vertical
+        [0, width, width * 2]
+      ]
+    },
+    {
+      name: "battleship",
+      directions: [
+        // horizontal
+        [0, 1, 2, 3],
+        // vertical
+        [0, width, width * 2, width * 3]
+      ]
+    },
+    {
+      name: "carrier",
+      directions: [
+        // horizontal
+        [0, 1, 2, 3, 4],
+        // vertical
+        [0, width, width * 2, width * 3, width * 4]
+      ]
+    }
+  ];
 });
